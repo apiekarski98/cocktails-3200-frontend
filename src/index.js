aa32ca876445f4d5df1ab02cacec521b127f66ac
Alek Piekarski <piekarski.a@husky.neu.edu>
2019-11-11 13:03:46 -0500

    Test frontend

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+async function getData() {
+    // const response = await fetch('https://swapi.co/api/people/1/');
+    const response = await fetch('https://cocktails-3200.herokuapp.com/');
+    const myJson = await response.json();
+    ReactDOM.render(<App name={JSON.stringify(myJson)}/>, document.getElementById('root'));
+}
+
+getData();
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
