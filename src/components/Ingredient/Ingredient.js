277628a21f86684b53cfc2739fb9f8e09c0148db
Alek Piekarski <piekarski.a@husky.neu.edu>
2019-11-11 18:05:25 -0500

    Fetch ingredients when navigating to ingredients page

--- src/components/Ingredient/Ingredient.js
+++ src/components/Ingredient/Ingredient.js
@@ -3,8 +3,23 @@ import './Ingredient.css';
 const { Component } = React;
 
 class Ingredient extends Component {
+    state = {
+        ingredients: [],
+    };
+
+    async getData() {
+        const response = await fetch('http://localhost:8000/api/ingredient');
+        const ingredients = await response.json();
+        console.log(ingredients);
+        this.setState({ ingredients});
+    }
+
+     componentDidMount() {
+        this.getData();
+    }
+
     render() {
-        return <h1 className="header">Ingredient</h1>;
+        return <h1 className="header">{JSON.stringify(this.state.ingredients)}</h1>;
     }
 }
 
