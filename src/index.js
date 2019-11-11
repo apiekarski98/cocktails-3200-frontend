6dfa94bafcbad65b4118d1f842099cef17fa8047
Alek Piekarski <piekarski.a@husky.neu.edu>
2019-11-11 13:46:58 -0500

    Restyled landing page

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-async function getData() {
-    // const response = await fetch('https://swapi.co/api/people/1/');
-    const response = await fetch('https://cocktails-3200.herokuapp.com/');
-    const myJson = await response.json();
-    ReactDOM.render(<App name={JSON.stringify(myJson)}/>, document.getElementById('root'));
-}
+// async function getData() {
+//     // const response = await fetch('https://swapi.co/api/people/1/');
+//     const response = await fetch('localhost:8000');
+//     const myJson = await response.json();
+//     ReactDOM.render(<App name={JSON.stringify(myJson)}/>, document.getElementById('root'));
+// }
+//
+// getData();
 
-getData();
-
-
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
