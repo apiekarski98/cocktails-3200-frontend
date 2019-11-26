5e914be60bbfa7d2499ee02af8389cd45e8e48da
Alek Piekarski <piekarski.a@husky.neu.edu>
2019-11-26 13:55:19 -0500

    Added read and delete functionality for ingredients

--- src/components/Ingredient/Ingredient.js
+++ src/components/Ingredient/Ingredient.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Ingredient.css';
+import ListItem from '../ListItem/ListItem';
 const { Component } = React;
 
 class Ingredient extends Component {
@@ -8,18 +9,36 @@ class Ingredient extends Component {
     };
 
     async getData() {
-        const response = await fetch('http://localhost:8000/api/ingredient');
-        const ingredients = await response.json();
-        console.log(ingredients);
-        this.setState({ ingredients});
+        try {
+            const response = await fetch('http://localhost:8000/api/ingredient');
+            const ingredients = await response.json();
+            this.setState({ ingredients });
+        } catch (err) {
+            return <p>There has been an issue loading the ingredients. Please try again</p>;
+        }
     }
 
      componentDidMount() {
         this.getData();
     }
 
+    onClick = async (ingredient_id) => {
+        try {
+            await fetch('http://localhost:8000/api/ingredient/' + ingredient_id,
+                {
+                    method: 'DELETE',
+                });
+            await this.getData();
+        } catch (err) {
+            throw err;
+        }
+    };
+
     render() {
-        return <h1 className="header">{JSON.stringify(this.state.ingredients)}</h1>;
+        const ingredients = this.state.ingredients;
+        return <h1 className="header">
+            {ingredients.map(i => <ListItem key={i.ingredient_id} text={i.ingredient_name} onClick={() => this.onClick(i.ingredient_id)}/>)}
+        </h1>;
     }
 }
 
