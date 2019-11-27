36b11caf0325c30506850109331ff2614a2b2850
Alek Piekarski <piekarski.a@husky.neu.edu>
2019-11-27 00:37:55 -0500

    Implement create functionality

--- src/components/Ingredient/Ingredient.js
+++ src/components/Ingredient/Ingredient.js
@@ -6,6 +6,7 @@ const { Component } = React;
 class Ingredient extends Component {
     state = {
         ingredients: [],
+        ingredient_name: "",
     };
 
     async getData() {
@@ -34,10 +35,44 @@ class Ingredient extends Component {
         }
     };
 
+    onClickAddIngredient = async () => {
+        if (this.state.ingredient_name !== null) {
+            try {
+                await fetch('http://localhost:8000/api/ingredient',
+                    {
+                        method: 'PUT',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            ingredient_name: this.state.ingredient_name,
+                        }),
+                    });
+                this.setState({ ingredient_name: "" });
+                await this.getData();
+            } catch (err) {
+                throw err;
+            }
+        }
+    };
+
+    handleNewIngredient = event => {
+        this.setState({ ingredient_name: event.target.value });
+    };
+
     render() {
         const ingredients = this.state.ingredients;
         return <h1 className="header">
+            <p>Ingredients</p>
             {ingredients.map(i => <ListItem key={i.ingredient_id} text={i.ingredient_name} onClick={() => this.onClick(i.ingredient_id)}/>)}
+            <p>Ingredient name: </p>
+            <input
+                type="text"
+                name="ingredient"
+                value={this.state.ingredient_name}
+                onChange={this.handleNewIngredient}/>
+            <button onClick={() => this.onClickAddIngredient()}>Add ingredient</button>
         </h1>;
     }
 }
